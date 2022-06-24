e3c6ae6b77e2a9f6f5240f5bf615936e1cf2ee7c
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2022-06-24 22:41:23 +0200

    new version

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giuliowaitforitdavide/recsyslearn',
-    version='0.1.2-alpha',
+    version='0.4.0-alpha',
     zip_safe=False,
 )
