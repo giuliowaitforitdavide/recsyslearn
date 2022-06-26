Fix mean in novelty computation.

recsyslearn/metrics.py
@@ -92,8 +92,8 @@ class Novelty(Metric):
 
 
 		top_n.loc[:, 'group'] = pd.to_numeric(top_n.loc[:,'group'])
-		top_n = top_n.groupby('user')['group'].apply(lambda x: - np.log2(1 / x))
-		return top_n.sum() / len(top_n)
+		top_n = top_n.groupby('user')['group'].apply(lambda x: np.mean(- np.log2(1 / x)))
+		return top_n.mean()
 
 
 class Entropy(Metric):