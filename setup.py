c5790865ca27de6e7c88e36fe127c2be24a34a9b
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2022-06-27 17:14:11 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giuliowaitforitdavide/recsyslearn',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
 )
