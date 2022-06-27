85a49180c6ad8b00d561691797145378235691f1
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2022-06-27 17:04:25 +0200

    fix: versioning

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giuliowaitforitdavide/recsyslearn',
-    version='version='0.4.1'',
+    version='0.4.0-alpha',
     zip_safe=False,
 )
