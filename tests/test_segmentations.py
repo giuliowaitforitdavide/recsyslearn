136f418296357e1b5e9a51517592b01186778c60
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2022-08-23 21:41:20 +0200

    feat: improved error tests

--- tests/test_segmentations.py
+++ tests/test_segmentations.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from recsyslearn.segmentations import ActivitySegmentation, PopularityPercentage, InteractionSegmentation, \
     DiscreteFeatureSegmentation
+from recsyslearn.errors import InvalidValueException, SegmentationNotSupportedException, WrongProportionsException
 from tests.utils import dataset_item_example, dataset_user_example, dataset_popularity, user_feature, item_feature, \
     user_error_feature
 from pandas.testing import assert_frame_equal
@@ -30,13 +31,17 @@ class InteractionSegmentationTest(unittest.TestCase):
             )
         )
 
-    @unittest.expectedFailure
     def test_segmentation_not_supported(self) -> None:
-        self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.1, 0.1, 0.1])
+        with self.assertRaises(SegmentationNotSupportedException) as context:
+            self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.1, 0.1, 0.1])
 
-    @unittest.expectedFailure
+        self.assertTrue('Number of supported group is between 1 and 3.' in str(context.exception))
+
     def test_segmentation_wrong_proportion(self) -> None:
-        self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.4])
+        with self.assertRaises(WrongProportionsException) as context:
+            self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.4])
+
+        self.assertTrue("Proportions doesn't cover all the items/users." in str(context.exception))
 
 
 class ItemPopularityPercentageTest(unittest.TestCase):
@@ -97,13 +102,17 @@ class ActivitySegmentationTest(unittest.TestCase):
             )
         )
 
-    @unittest.expectedFailure
     def test_segmentation_not_supported(self) -> None:
-        self.user_segmenter.segment(dataset_user_example, [0.7, 0.1, 0.1, 0.1])
+        with self.assertRaises(SegmentationNotSupportedException) as context:
+            self.user_segmenter.segment(dataset_user_example, [0.7, 0.1, 0.1, 0.1])
 
-    @unittest.expectedFailure
+        self.assertTrue('Number of supported group is between 1 and 3.' in str(context.exception))
+
     def test_segmentation_wrong_proportion(self) -> None:
-        self.user_segmenter.segment(dataset_user_example, [0.7, 0.4])
+        with self.assertRaises(WrongProportionsException) as context:
+            self.user_segmenter.segment(dataset_user_example, [0.7, 0.4])
+
+        self.assertTrue("Proportions doesn't cover all the items/users." in str(context.exception))
 
 
 class UserDiscreteFeatureSegmentationTest(unittest.TestCase):
@@ -125,9 +134,11 @@ class UserDiscreteFeatureSegmentationTest(unittest.TestCase):
         self.assertTrue(len(np.intersect1d(segmented_groups.loc[segmented_groups['user'] == '3'].group,
                                            segmented_groups.loc[segmented_groups['user'] == '4'].group)) == 1)
 
-    @unittest.expectedFailure
     def test_segmentation_invalid_value(self) -> None:
-        self.feature_segmenter.segment(feature=user_error_feature)
+        with self.assertRaises(InvalidValueException) as context:
+            self.feature_segmenter.segment(feature=user_error_feature)
+
+        self.assertTrue('Feature contains -1 as value. Please select another fill_na value.' in str(context.exception))
 
 
 class ItemDiscreteFeatureSegmentationTest(unittest.TestCase):
