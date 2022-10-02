ec9c697ea0b778e6e3ced999d8c1208bef640ec7
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2022-10-03 00:40:44 +0200

    fix: job workflows, type on some functions. Add docs next days

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -13,17 +13,14 @@ class EntropyTest(unittest.TestCase):
     Tester for the Entropy class.
     """
 
-    def setUp(self) -> None:
-        self.evaluator = Entropy()
-
     def test_user_exposure(self) -> None:
         top_n = first_example.merge(user_groups, on='user')
-        entropy = self.evaluator.evaluate(top_n)
+        entropy = Entropy().evaluate(top_n)
         self.assertAlmostEqual(entropy, 0.91830, delta=1e-5)
 
     def test_item_exposure(self) -> None:
         top_n = first_example.merge(item_groups, on='item')
-        entropy = self.evaluator.evaluate(top_n)
+        entropy = Entropy().evaluate(top_n)
         self.assertAlmostEqual(entropy, 1.48547, delta=1e-5)
 
 
@@ -32,21 +29,18 @@ class KullbackLeiblerTest(unittest.TestCase):
     Tester for the KullbackLeibler class.
     """
 
-    def setUp(self) -> None:
-        self.evaluator = KullbackLeibler()
-
     def test_user_effectiveness(self) -> None:
         top_n = second_example.merge(user_groups, on='user')
         rel_matrix = rel_matrix_1.merge(user_groups, on='user')
         target_representation = pd.DataFrame([['1', 0.5], ['2', 0.5]], columns=['group', 'target_representation'])
-        divergence = self.evaluator.evaluate(top_n, target_representation, rel_matrix)
+        divergence = KullbackLeibler().evaluate(top_n, target_representation, rel_matrix)
         self.assertAlmostEqual(divergence, 0.08530, delta=1e-5)
 
     def test_item_exposure(self) -> None:
         top_n = first_example.merge(item_groups, on='item')
         target_representation = pd.DataFrame([['1', 0.2], ['2', 0.3], ['3', 0.5]],
                                              columns=['group', 'target_representation'])
-        divergence = self.evaluator.evaluate(top_n, target_representation)
+        divergence = KullbackLeibler().evaluate(top_n, target_representation)
         self.assertAlmostEqual(divergence, 0, delta=1e-5)
 
 
@@ -55,23 +49,20 @@ class MutualInformationTest(unittest.TestCase):
     Tester for the KullbackLeibler class.
     """
 
-    def setUp(self) -> None:
-        self.evaluator = MutualInformation()
-
     def test_user_exposure(self) -> None:
         top_n = first_example.merge(user_groups, on='user')
-        mi = self.evaluator.evaluate(top_n, 'user')
+        mi = MutualInformation().evaluate(top_n, 'user')
         self.assertAlmostEqual(mi, 0.25582, delta=1e-5)
 
     def test_item_exposure(self) -> None:
         top_n = first_example.merge(item_groups, on='item')
-        mi = self.evaluator.evaluate(top_n, 'item')
+        mi = MutualInformation().evaluate(top_n, 'item')
         self.assertAlmostEqual(mi, 0.10570, delta=1e-5)
 
     def test_flag_not_valid(self) -> None:
         with self.assertRaises(FlagNotValidException) as context:
             top_n = first_example.merge(item_groups, on='item')
-            self.evaluator.evaluate(top_n, 'ratings')
+            MutualInformation().evaluate(top_n, 'ratings')
 
         self.assertTrue('Invalid flag.' in str(context.exception))
 
@@ -81,20 +72,17 @@ class CoverageTest(unittest.TestCase):
     Tester for the Coverage class.
     """
 
-    def setUp(self) -> None:
-        self.evaluator = Coverage()
-
     def test_coverage_one(self) -> None:
         top_n = first_example[
             (first_example['item'] != '3') & (first_example['item'] != '4') & (first_example['item'] != '1')]
-        cov = self.evaluator.evaluate(top_n, item_groups.item.tolist())
+        cov = Coverage().evaluate(top_n, item_groups.item.tolist())
         self.assertAlmostEqual(cov, 0.7)
 
     def test_coverage_two(self) -> None:
         top_n = first_example[
             (first_example['item'] != '1') & (first_example['item'] != '2') & (first_example['item'] != '4') & (
                 first_example['item'] != '7') & (first_example['item'] != '9') & (first_example['item'] != '10')]
-        cov = self.evaluator.evaluate(top_n, item_groups.item.tolist())
+        cov = Coverage().evaluate(top_n, item_groups.item.tolist())
         self.assertAlmostEqual(cov, 0.4)
 
 
@@ -104,23 +92,22 @@ class NoveltyTest(unittest.TestCase):
     """
 
     def setUp(self):
-        self.evaluator = Novelty()
         novelty = np.vectorize(lambda x: -np.log2(x))
         self.novelty = lambda list: float(np.mean(novelty(list)))
 
     def test_novelty_one(self) -> None:
         top_n = first_example.merge(item_groups, on='item')
-        nov = self.evaluator.evaluate(top_n)
+        nov = Novelty().evaluate(top_n)
         self.assertAlmostEqual(nov, self.novelty(top_n['group'].to_numpy()), delta=1e-5)
 
     def test_novelty_two(self) -> None:
         top_n = second_example.merge(item_groups, on='item')
-        nov = self.evaluator.evaluate(top_n)
+        nov = Novelty().evaluate(top_n)
         self.assertAlmostEqual(nov, self.novelty(top_n['group'].to_numpy()), delta=1e-5)
 
     def test_novelty_three(self) -> None:
         top_n = first_example.merge(item_pop_perc, on='item')
-        nov = self.evaluator.evaluate(top_n, popularity_definition='percentage')
+        nov = Novelty().evaluate(top_n, popularity_definition='percentage')
         self.assertAlmostEqual(nov, self.novelty(top_n['percentage'].to_numpy()), delta=1e-5)
 
 
@@ -129,11 +116,8 @@ class NDCGTest(unittest.TestCase):
     Tester for the NDCG class.
     """
 
-    def setUp(self) -> None:
-        self.evaluator = NDCG()
-
     def test_ndcg_one(self) -> None:
-        ndcg_df = self.evaluator.evaluate(top_n_1, pos_items, ats=(2,))
+        ndcg_df = NDCG().evaluate(top_n_1, pos_items, ats=(2,))
         ndcg_vals = pd.DataFrame.from_dict(
             {
                 '1': (1 / np.log(1 + 1) + 0 / np.log(2 + 1)) / (1 / np.log(1 + 1) + 1 / np.log(2 + 1)),  # OK
--- tests/test_segmentations.py
+++ tests/test_segmentations.py
@@ -13,33 +13,30 @@ class InteractionSegmentationTest(unittest.TestCase):
     Tester for the InteractionSegmentation class.
     """
 
-    def setUp(self) -> None:
-        self.interaction_segmenter = InteractionSegmentation()
-
     def test_segmentation(self) -> None:
-        segmented_groups = self.interaction_segmenter.segment(dataset_item_example)
+        segmented_groups = InteractionSegmentation().segment(dataset_item_example)
         self.assertTrue(segmented_groups.loc[segmented_groups['item'] == '1', 'group'].eq('1').all())
         self.assertTrue(segmented_groups.loc[segmented_groups['item'] == '2', 'group'].eq('2').all())
 
     def test_segmentation_mantissa(self) -> None:
-        self.interaction_segmenter.segment(dataset_item_example, [0.6, 0.3, 0.1])
+        InteractionSegmentation().segment(dataset_item_example, [0.6, 0.3, 0.1])
 
     def test_segmentation_entire_dataset(self) -> None:
         self.assertIsNone(
             assert_frame_equal(
-                dataset_item_example, self.interaction_segmenter.segment(dataset_item_example, [1])
+                dataset_item_example, InteractionSegmentation().segment(dataset_item_example, [1])
             )
         )
 
     def test_segmentation_not_supported(self) -> None:
         with self.assertRaises(SegmentationNotSupportedException) as context:
-            self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.1, 0.1, 0.1])
+            InteractionSegmentation().segment(dataset_item_example, [0.7, 0.1, 0.1, 0.1])
 
         self.assertTrue('Number of supported group is between 1 and 3.' in str(context.exception))
 
     def test_segmentation_wrong_proportion(self) -> None:
         with self.assertRaises(WrongProportionsException) as context:
-            self.interaction_segmenter.segment(dataset_item_example, [0.7, 0.4])
+            InteractionSegmentation().segment(dataset_item_example, [0.7, 0.4])
 
         self.assertTrue("Proportions doesn't cover all the items/users." in str(context.exception))
 
@@ -49,11 +46,8 @@ class ItemPopularityPercentageTest(unittest.TestCase):
     Test for the PopularityPercentage class
     """
 
-    def setUp(self) -> None:
-        self.item_popularity_segmenter = PopularityPercentage()
-
     def test_popularity(self) -> None:
-        popularity_dataframe = self.item_popularity_segmenter.segment(dataset_popularity)
+        popularity_dataframe = PopularityPercentage().segment(dataset_popularity)
         self.assertTrue((popularity_dataframe['percentage'] < 1.).all())
         self.assertAlmostEqual(1., popularity_dataframe['percentage'].sum(), delta=1e-5)
         self.assertTrue(popularity_dataframe.loc[popularity_dataframe['item'] == '1', 'percentage'].eq(0.6).all())
@@ -66,11 +60,8 @@ class UserPopularityPercentageTest(unittest.TestCase):
     Test for the PopularityPercentage class
     """
 
-    def setUp(self) -> None:
-        self.user_popularity_segmenter = PopularityPercentage()
-
     def test_popularity(self) -> None:
-        popularity_dataframe = self.user_popularity_segmenter.segment(dataset_popularity, group='user')
+        popularity_dataframe = PopularityPercentage().segment(dataset_popularity, group='user')
         self.assertTrue((popularity_dataframe['percentage'] < 1.).all())
         self.assertAlmostEqual(1., popularity_dataframe['percentage'].sum(), delta=1e-5)
         self.assertTrue(popularity_dataframe.loc[popularity_dataframe['user'] == '1', 'percentage'].eq(0.7).all())
@@ -83,30 +74,27 @@ class ActivitySegmentationTest(unittest.TestCase):
     Test for the ActivitySegmentation class.
     """
 
-    def setUp(self) -> None:
-        self.user_segmenter = ActivitySegmentation()
-
     def test_segmentation(self) -> None:
-        segmented_groups = self.user_segmenter.segment(dataset_user_example)
+        segmented_groups = ActivitySegmentation().segment(dataset_user_example)
         self.assertTrue(segmented_groups.loc[segmented_groups['user'] == '1', 'group'].eq('1').all())
         self.assertTrue(segmented_groups.loc[segmented_groups['user'] == '2', 'group'].eq('2').all())
         self.assertTrue(segmented_groups.loc[segmented_groups['user'] == '3', 'group'].eq('2').all())
 
     def test_segmentation_mantissa(self) -> None:
-        self.user_segmenter.segment(dataset_user_example, [0.6, 0.3, 0.1])
+        ActivitySegmentation().segment(dataset_user_example, [0.6, 0.3, 0.1])
 
     def test_segmentation_entire_dataset(self) -> None:
-        self.assertIsNone(assert_frame_equal(dataset_user_example, self.user_segmenter.segment(dataset_user_example, [1])))
+        self.assertIsNone(assert_frame_equal(dataset_user_example, ActivitySegmentation().segment(dataset_user_example, [1])))
 
     def test_segmentation_not_supported(self) -> None:
         with self.assertRaises(SegmentationNotSupportedException) as context:
-            self.user_segmenter.segment(dataset_user_example, [0.7, 0.1, 0.1, 0.1])
+            ActivitySegmentation().segment(dataset_user_example, [0.7, 0.1, 0.1, 0.1])
 
         self.assertTrue('Number of supported group is between 1 and 3.' in str(context.exception))
 
     def test_segmentation_wrong_proportion(self) -> None:
         with self.assertRaises(WrongProportionsException) as context:
-            self.user_segmenter.segment(dataset_user_example, [0.7, 0.4])
+            ActivitySegmentation().segment(dataset_user_example, [0.7, 0.4])
 
         self.assertTrue("Proportions doesn't cover all the items/users." in str(context.exception))
 
@@ -116,11 +104,8 @@ class UserDiscreteFeatureSegmentationTest(unittest.TestCase):
     Tester for the DiscreteFeatureSegmentation class, on user features.
     """
 
-    def setUp(self) -> None:
-        self.feature_segmenter = DiscreteFeatureSegmentation()
-
     def test_segmentation(self) -> None:
-        segmented_groups = self.feature_segmenter.segment(feature=user_feature)
+        segmented_groups = DiscreteFeatureSegmentation().segment(feature=user_feature)
         self.assertTrue(len(np.intersect1d(segmented_groups.loc[segmented_groups['user'] == '1'].group,
                                            segmented_groups.loc[segmented_groups['user'] == '2'].group)) == 0)
         self.assertTrue(len(np.intersect1d(segmented_groups.loc[segmented_groups['user'] == '1'].group,
@@ -132,7 +117,7 @@ class UserDiscreteFeatureSegmentationTest(unittest.TestCase):
 
     def test_segmentation_invalid_value(self) -> None:
         with self.assertRaises(InvalidValueException) as context:
-            self.feature_segmenter.segment(feature=user_error_feature)
+            DiscreteFeatureSegmentation().segment(feature=user_error_feature)
 
         self.assertTrue('Feature contains -1 as value. Please select another fill_na value.' in str(context.exception))
 
@@ -142,11 +127,8 @@ class ItemDiscreteFeatureSegmentationTest(unittest.TestCase):
     Tester for the DiscreteFeatureSegmentation class, on item features.
     """
 
-    def setUp(self) -> None:
-        self.feature_segmenter = DiscreteFeatureSegmentation()
-
     def test_segmentation(self) -> None:
-        segmented_groups = self.feature_segmenter.segment(feature=item_feature)
+        segmented_groups = DiscreteFeatureSegmentation().segment(feature=item_feature)
         self.assertTrue(len(np.intersect1d(segmented_groups.loc[segmented_groups['item'] == '1'].group,
                                            segmented_groups.loc[segmented_groups['item'] == '2'].group)) == 1)
         self.assertTrue(len(np.intersect1d(segmented_groups.loc[segmented_groups['item'] == '1'].group,
