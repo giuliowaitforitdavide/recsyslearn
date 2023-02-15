a252afa822eb5ce30037ca29e61f5dd37ddba6ca
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2023-02-15 15:20:31 +0100

    Bump version: 1.0.0 → 1.0.1

--- recsyslearn/__init__.py
+++ recsyslearn/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Giulio Davide Carparelli"""
 __email__ = 'giulio.davide.97@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url='https://github.com/giuliowaitforitdavide/recsyslearn',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
