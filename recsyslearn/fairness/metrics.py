af6eccbf6f8429a834f134aead995c79bf08b5e9
giuliowaitforitdavide <giulio@fiscozen.it>
2023-11-08 19:04:15 +0100

    ci: fix pipeline and update dependencies

--- recsyslearn/fairness/metrics.py
+++ recsyslearn/fairness/metrics.py
@@ -46,14 +46,13 @@ class Entropy(FairnessMetric):
         The computed entropy.
         """
 
-        check_columns_exist(top_n, ['user', 'item', 'rank', 'group'])
+        check_columns_exist(top_n, ["user", "item", "rank", "group"])
 
-        top_n = eff_matrix(
-            top_n, rel_matrix) if rel_matrix is not None else top_n
+        top_n = eff_matrix(top_n, rel_matrix) if rel_matrix is not None else top_n
         top_n = prob_matrix(top_n)
-        top_n = top_n[['group', 'rank']].groupby('group', as_index=False).sum()
-        top_n['rank'] = top_n['rank'] * np.log2(top_n['rank'])
-        return - top_n['rank'].sum()
+        top_n = top_n[["group", "rank"]].groupby("group", as_index=False).sum()
+        top_n["rank"] = top_n["rank"] * np.log2(top_n["rank"])
+        return -top_n["rank"].sum()
 
 
 class KullbackLeibler(FairnessMetric):
@@ -62,8 +61,12 @@ class KullbackLeibler(FairnessMetric):
     Kullback-Leibler divergence evaluator for recommender systems.
     """
 
-    def evaluate(self, top_n: pd.DataFrame, target_representation: pd.DataFrame,
-                 rel_matrix: pd.DataFrame = None) -> float:
+    def evaluate(
+        self,
+        top_n: pd.DataFrame,
+        target_representation: pd.DataFrame,
+        rel_matrix: pd.DataFrame = None,
+    ) -> float:
         """
         Compute the Kullback-Leibler divergence of a model, for a given target representation, by using its recommendation list.
 
@@ -91,18 +94,21 @@ class KullbackLeibler(FairnessMetric):
         The computed KL Divergence for the given target representation.
         """
 
-        check_columns_exist(top_n, ['user', 'item', 'rank', 'group'])
-        check_columns_exist(target_representation, [
-                            'group', 'target_representation'])
+        check_columns_exist(top_n, ["user", "item", "rank", "group"])
+        check_columns_exist(target_representation, ["group", "target_representation"])
 
-        top_n = eff_matrix(
-            top_n, rel_matrix) if rel_matrix is not None else exp_matrix(top_n)
+        top_n = (
+            eff_matrix(top_n, rel_matrix)
+            if rel_matrix is not None
+            else exp_matrix(top_n)
+        )
         top_n = prob_matrix(top_n)
-        top_n = top_n[['group', 'rank']].groupby('group', as_index=False).sum()
-        top_n = top_n.merge(target_representation, on='group')
-        top_n['rank'] = top_n['rank'] * \
-            np.log2(top_n['rank'] / top_n['target_representation'])
-        return top_n['rank'].sum()
+        top_n = top_n[["group", "rank"]].groupby("group", as_index=False).sum()
+        top_n = top_n.merge(target_representation, on="group")
+        top_n["rank"] = top_n["rank"] * np.log2(
+            top_n["rank"] / top_n["target_representation"]
+        )
+        return top_n["rank"].sum()
 
 
 class MutualInformation(FairnessMetric):
@@ -111,7 +117,9 @@ class MutualInformation(FairnessMetric):
     Mutual Information evaluator for recommender systems.
     """
 
-    def evaluate(self, top_n: pd.DataFrame, flag: str, rel_matrix: pd.DataFrame = None) -> float:
+    def evaluate(
+        self, top_n: pd.DataFrame, flag: str, rel_matrix: pd.DataFrame = None
+    ) -> float:
         """
         Compute the Mutual Information of a model by using its recommendation list.
 
@@ -140,23 +148,34 @@ class MutualInformation(FairnessMetric):
         The computed Mutual Information.
         """
 
-        check_columns_exist(top_n, ['user', 'item', 'rank', 'group'])
+        check_columns_exist(top_n, ["user", "item", "rank", "group"])
 
-        not_flagged = {'user': 'item', 'item': 'user'}
+        not_flagged = {"user": "item", "item": "user"}
 
-        top_n = eff_matrix(
-            top_n, rel_matrix) if rel_matrix is not None else exp_matrix(top_n)
+        top_n = (
+            eff_matrix(top_n, rel_matrix)
+            if rel_matrix is not None
+            else exp_matrix(top_n)
+        )
         top_n = prob_matrix(top_n)
         not_grouped = not_flagged.get(flag)
-        P_xy = top_n[[not_grouped, 'group', 'rank']].groupby(
-            [not_grouped, 'group'], as_index=False).sum()
-        P_xP_y = top_n[[not_grouped, 'group', 'rank']].groupby(
-            not_grouped, as_index=False).sum()
-        P_xP_y = P_xy[[not_grouped, 'group']].merge(P_xP_y, on=not_grouped)
-        tmp = top_n[['group', 'rank']].groupby('group', as_index=False).sum()
-        P_xP_y = P_xP_y.merge(tmp, on=['group'])
-        P_xP_y['rank'] = P_xP_y['rank_x'] * P_xP_y['rank_y']
-        tmp = P_xP_y[[not_grouped, 'group', 'rank']].merge(
-            P_xy, on=[not_grouped, 'group'])
-        tmp['rank'] = tmp['rank_y'] * np.log2(tmp['rank_y'] / tmp['rank_x'])
-        return tmp['rank'].sum()
+        P_xy = (
+            top_n[[not_grouped, "group", "rank"]]
+            .groupby([not_grouped, "group"], as_index=False)
+            .sum()
+        )
+        P_xP_y = (
+            top_n[[not_grouped, "group", "rank"]]
+            .groupby(not_grouped, as_index=False)
+            .agg({"rank": "sum"})
+        )
+        print(P_xP_y.head())
+        P_xP_y = P_xy[[not_grouped, "group"]].merge(P_xP_y, on=not_grouped)
+        tmp = top_n[["group", "rank"]].groupby("group", as_index=False).sum()
+        P_xP_y = P_xP_y.merge(tmp, on=["group"])
+        P_xP_y["rank"] = P_xP_y["rank_x"] * P_xP_y["rank_y"]
+        tmp = P_xP_y[[not_grouped, "group", "rank"]].merge(
+            P_xy, on=[not_grouped, "group"]
+        )
+        tmp["rank"] = tmp["rank_y"] * np.log2(tmp["rank_y"] / tmp["rank_x"])
+        return tmp["rank"].sum()
