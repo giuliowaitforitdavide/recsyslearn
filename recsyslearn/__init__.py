3b32df54f7e56a08057ca5c1640487f47ce973d3
giuliowaitforitdavide <giulio@fiscozen.it>
2023-12-20 17:36:42 +0100

    Bump version: 1.0.1 → 2.0.0

--- recsyslearn/__init__.py
+++ recsyslearn/__init__.py
@@ -2,4 +2,4 @@ from __future__ import annotations
 
 __author__ = "Giulio Davide Carparelli"
 __email__ = "giulio.davide.97@gmail.com"
-__version__ = "1.0.1"
+__version__ = "2.0.0"
--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giuliowaitforitdavide/recsyslearn",
-    version="1.0.1",
+    version="2.0.0",
     zip_safe=False,
 )
